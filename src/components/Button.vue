08bfac3f86c2393b6b1acb2fb3e8acc676805c0d
ymoretto <ymoretto@gmail.com>
2021-10-10 21:22:20 +0200

    1

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="button">
+        <button>{{message}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Button",
+
+    props: {
+        message: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
